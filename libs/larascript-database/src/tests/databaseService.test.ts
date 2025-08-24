6fe5f1981aa04c7ec80ba2102b3c69818f3df11d
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-25 00:43:26 +0100

    test: enhance databaseService tests with additional connection and adapter validation scenarios

--- libs/larascript-database/src/tests/databaseService.test.ts
+++ libs/larascript-database/src/tests/databaseService.test.ts
@@ -7,85 +7,125 @@ import { MockSQLAdapter } from "./database/mocks/MockSQLAdapter";
 process.on('unhandledRejection', (reason) => {
     console.log(reason); // log the reason including the stack trace
     throw reason;
-  });
+});
 
 describe("Database Service", () => {
+    const DEFAULT_CONNECTION = 'default'
+    const MOCK_SQL_OPTIONS: ReturnType<MockSQLAdapter['getConfig']> = {
+        connectionString: 'sql://user:pass@localhost:3306/db'
+    }
+
     let databaseService: DatabaseService
     let defaultConfig: IDatabaseConfig = {
-        defaultConnectionName: 'default',
+        defaultConnectionName: DEFAULT_CONNECTION,
         keepAliveConnections: '',
     }
 
     beforeEach(() => {
         databaseService = new DatabaseService(defaultConfig)
-  });
+    });
 
-  describe("getConfig", () => {
-    test("should return the config", () => {
-      const config = databaseService.getConfig()
+    describe("getConfig", () => {
+        test("should return the config", () => {
+            const config = databaseService.getConfig()
 
-      expect(config).toBeDefined()
-      expect(config.defaultConnectionName).toBe('default')
-      expect(config.keepAliveConnections).toBe('')
-    })
-  });
-
-  describe("connectionConfig", () => {
-    test("should add a connection", () => {
-        databaseService.addConnection('sql', MockSQLAdapter,  {
-            connectionString: 'sql://user:pass@localhost:3306/db'
+            expect(config).toBeDefined()
+            expect(config.defaultConnectionName).toBe(DEFAULT_CONNECTION)
+            expect(config.keepAliveConnections).toBe('')
         })
-    
-        expect(databaseService.getConnectionConfig('sql')).toBeDefined()
-        expect(databaseService.getConnectionConfig('sql').connectionString).toBe('sql://user:pass@localhost:3306/db')
-        expect(databaseService.getAdapter('sql')).toBeInstanceOf(MockSQLAdapter)
-      })    
-  });
+    });
 
-  describe("boot", () => {
-    
-    test("should fail when no connection is added", async () => {
-        await expect(databaseService.boot()).rejects.toThrow(DatabaseConnectionException)
-    })
-    
-    test("should connect to the default connection", async () => {
-        databaseService.addConnection('default', MockSQLAdapter,  {
-            connectionString: 'sql://user:pass@localhost:3306/db'
-        })
-        
-        jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
+    describe("connectionConfig", () => {
+        test("should add a connection", () => {
+            databaseService.addConnection('sql', MockSQLAdapter, MOCK_SQL_OPTIONS)
 
-        await databaseService.boot()
-
-        expect(databaseService.connectDefault).toHaveBeenCalled()
-    })
-
-    test("should not connect to the default connection if onBootConnect is false", async () => {
-        const databaseService = new DatabaseService({
-            ...defaultConfig,
-            onBootConnect: false
+            expect(databaseService.getConnectionConfig('sql')).toBeDefined()
+            expect(databaseService.getConnectionConfig('sql').connectionString).toBe('sql://user:pass@localhost:3306/db')
+            expect(databaseService.getAdapter('sql')).toBeInstanceOf(MockSQLAdapter)
         })
 
-        jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
-        
-        await databaseService.boot()
-
-        expect(databaseService.connectDefault).not.toHaveBeenCalled()
-    })
-
-    test("should connect keep alive with other connections defined", async () => {
-        const databaseService = new DatabaseService({
-            ...defaultConfig,
-            keepAliveConnections: 'other',
+        test("should return default connection", () => {
+            expect(databaseService.getDefaultConnectionName()).toBe(DEFAULT_CONNECTION)
         })
 
-        jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
-        jest.spyOn(databaseService, 'connectAdapter').mockImplementation(() => Promise.resolve())
+        test("should override connection name and return override value", () => {
+            databaseService.setDefaultConnectionName('change')
 
-        await databaseService.boot()
+            expect(databaseService.getDefaultConnectionName()).toBe('change')
+        })
+    });
 
-        expect(databaseService.connectAdapter).toHaveBeenCalledWith('other')
+    describe("adapters", () => {
+        test("should return expected adapter", () => {
+            databaseService.addConnection(DEFAULT_CONNECTION, MockSQLAdapter, MOCK_SQL_OPTIONS)
+
+            const adapter = databaseService.getAdapter(DEFAULT_CONNECTION)
+
+            expect(adapter).toBeInstanceOf(MockSQLAdapter)
+        })
+
+        test("should throw an error for adapter that doesn't exist", () => {
+            databaseService.addConnection(DEFAULT_CONNECTION, MockSQLAdapter, MOCK_SQL_OPTIONS)
+
+            expect(() => databaseService.getAdapter('bad-adapter')).toThrow('Adapter not found: bad-adapter')
+        })
     })
-  })
+
+    describe("boot", () => {
+
+        test("should fail when no connection is added", async () => {
+            await expect(databaseService.boot()).rejects.toThrow(DatabaseConnectionException)
+        })
+
+        test("should connect to the default connection", async () => {
+            databaseService.addConnection(DEFAULT_CONNECTION, MockSQLAdapter, {
+                connectionString: 'sql://user:pass@localhost:3306/db'
+            })
+
+            jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
+
+            await databaseService.boot()
+
+            expect(databaseService.connectDefault).toHaveBeenCalled()
+        })
+
+        test("should not connect to the default connection if onBootConnect is false", async () => {
+            const databaseService = new DatabaseService({
+                ...defaultConfig,
+                onBootConnect: false
+            })
+
+            jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
+
+            await databaseService.boot()
+
+            expect(databaseService.connectDefault).not.toHaveBeenCalled()
+        })
+
+        test("should connect keep alive with other connections defined", async () => {
+            const databaseService = new DatabaseService({
+                ...defaultConfig,
+                keepAliveConnections: 'other',
+            })
+
+            jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
+            jest.spyOn(databaseService, 'connectAdapter').mockImplementation(() => Promise.resolve())
+
+            await databaseService.boot()
+
+            expect(databaseService.connectAdapter).toHaveBeenCalledWith('other')
+        })
+
+        test("should throw an error on connect keep alive with missing connection", async () => {
+            const databaseService = new DatabaseService({
+                ...defaultConfig,
+                keepAliveConnections: 'other',
+            })
+
+            jest.spyOn(databaseService, 'connectDefault').mockImplementation(() => Promise.resolve())
+
+            await expect(databaseService.boot()).rejects.toThrow(DatabaseConnectionException)
+        })
+    })
 
 });

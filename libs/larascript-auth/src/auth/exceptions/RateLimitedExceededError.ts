f9e810c0534f1d5d022e3040863bfea17e5605a2
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-24 14:48:59 +0100

    chore: remove RateLimitedExceededError class from larascript-auth package as it is no longer needed

--- libs/larascript-auth/src/auth/exceptions/RateLimitedExceededError.ts
+++
@@ -1,6 +0,0 @@
-export class RateLimitedExceededError extends Error {
-  constructor(message: string = "Too many requests. Try again later.") {
-    super(message);
-    this.name = "RateLimitedExceededError";
-  }
-}

cceb010f73fb20fb7a691f6f0bd504770da1f02e
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-01 21:47:26 +0100

    refactor: export IUploadedFile interface

--- libs/larascript-storage/src/storage/interfaces/index.ts
+++ libs/larascript-storage/src/storage/interfaces/index.ts
@@ -3,4 +3,6 @@ export * from "./IStorageAdapters.t";
 export * from "./IStorageConfig.t";
 export * from "./IStorageFile";
 export * from "./IStorageService.t";
+export * from "./IUploadedFile.t";
 export * from "./meta";
+

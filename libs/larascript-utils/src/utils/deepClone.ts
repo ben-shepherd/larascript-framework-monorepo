ec947be87d6fd53419e952a54916aaa045243b29
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-21 20:25:28 +0100

    refactor: update deepClone function to use default import from lodash and add JSDoc comments

--- libs/larascript-utils/src/utils/deepClone.ts
+++ libs/larascript-utils/src/utils/deepClone.ts
@@ -1,5 +1,10 @@
-import * as _ from "lodash";
+import _ from "lodash";
 
+/**
+ * Deep clone an object
+ * @param obj - The object to clone
+ * @returns The cloned object
+ */
 export function deepClone<T>(obj: T): T {
   return _.cloneDeep(obj) as T;
 }

3cbb34078cc2d9501fc7280432de25a0db441b8f
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-21 13:05:09 +0100

    feat: add output exports to test-helpers package for enhanced module accessibility

--- libs/test-helpers/src/index.ts
+++ libs/test-helpers/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./output/index.js";
 export * from "./test-helpers/BaseInMemoryRepository.js";
 export * from "./test-helpers/BaseModel.js";
 export * from "./test-helpers/CustomSequencer.js";

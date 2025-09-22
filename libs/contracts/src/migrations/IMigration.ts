34c7c0338fa6e9eecc932ee02aaf4f9a09aabec9
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-22 10:56:59 +0100

    refactor: update migration import paths to use specific database module and remove unused dependencies

--- libs/contracts/src/migrations/IMigration.ts
+++ libs/contracts/src/migrations/IMigration.ts
@@ -1,4 +1,4 @@
-import { IDatabaseAdapter } from "@/database/index.js";
+import { IDatabaseAdapter } from "@/database/database/adapter.t.js";
 import { TClassConstructor } from "@larascript-framework/larascript-utils";
 
 /**

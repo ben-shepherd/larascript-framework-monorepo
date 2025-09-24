8bc5116891a42ace382e488d86da1e7a5b2a2bef
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-24 21:01:44 +0100

    feat: initialize test-database package with configuration files, Docker setup, and basic test structure

---
+++ libs/test-database/src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { DatabaseConfig, IDatabaseConfig } from "@larascript-framework/larascript-database";
+
+export const databaseConfig: IDatabaseConfig = {
+    defaultConnectionName: "postgres",
+    keepAliveConnections: "",
+    connections: [
+        DatabaseConfig.postgres("postgres", {
+            uri: "postgres://root:example@localhost:5432/app",
+            options: {},
+        }),
+    ],
+}

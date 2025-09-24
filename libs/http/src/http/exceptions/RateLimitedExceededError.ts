94f7816db52d059f0be0ba9e6663abb4957f7999
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-24 15:59:55 +0100

    feat: introduce AbstractMiddlewareException class and refactor RateLimitedExceededError to extend it; update SecurityMiddleware to handle exceptions properly

--- libs/http/src/http/exceptions/RateLimitedExceededError.ts
+++ libs/http/src/http/exceptions/RateLimitedExceededError.ts
@@ -1,4 +1,8 @@
-export class RateLimitedExceededError extends Error {
+import { AbstractMiddlewareException } from "../base/AbstractMiddlewareException.js";
+
+export class RateLimitedExceededError extends AbstractMiddlewareException {
+    code = 429;
+    
     constructor(message: string = "Too many requests. Try again later.") {
       super(message);
       this.name = "RateLimitedExceededError";

e52b3ac51f14025ca77936a3f7a4c9a378772b05
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-26 14:59:21 +0100

    test: add unit tests for multiple field sorting and default results handling in SortOptions

--- libs/larascript-http/src/tests/sorting.test.ts
+++ libs/larascript-http/src/tests/sorting.test.ts
@@ -1,5 +1,7 @@
 import SortOptions from "@/http/utils/SortOptions.js";
 import { describe, expect, test } from "@jest/globals";
+import { Request } from "express";
+import { TSortDefaults } from "node_modules/@larascript-framework/contracts/dist/http/ISorting.js";
 
 describe("Sorting Test Suite", () => {
   describe("Direction Normalization", () => {
@@ -20,4 +22,41 @@ describe("Sorting Test Suite", () => {
       expect(value5).toBe('asc')
     })
   });
+
+  test('multiple fields should be normalized', () => {
+    const mockRequest = {
+        query: {
+            sort: {
+                name: 'asc',
+                age: 'desc'
+            }
+        }
+    } as unknown as Request
+
+    const value = SortOptions.parseRequest(mockRequest)
+    
+    expect(value.results).toEqual({
+      name: 'asc',
+      age: 'desc'
+    })
+  })
+
+  describe('default results', () => {
+    test('should create the default results when no sort query is present', () => {
+        const mockRequest = {
+            query: {}
+        } as unknown as Request
+    
+        const defaultSortOptions: TSortDefaults = {
+            defaultField: 'createdAt',
+            defaultDirection: 'asc'
+        }
+    
+        const value = SortOptions.parseRequest(mockRequest, defaultSortOptions)
+    
+        expect(value.results).toEqual({
+          createdAt: 'asc'
+        })
+      })
+  })
 });

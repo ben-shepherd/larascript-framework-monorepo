3ccd5e402ed95e93d6a7db3c254625726f1cdbea
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-10-11 00:28:49 +0100

    test: refactor migration tests, simplified up/down tables and handle references to related tables in test migration file

--- apps/larascript-framework/src/tests/larascript/migration/migration.test.ts
+++ apps/larascript-framework/src/tests/larascript/migration/migration.test.ts
@@ -1,12 +1,10 @@
 /* eslint-disable no-undef */
 import { app } from '@/core/services/App.js';
 import TestMigrationModel from '@/tests/larascript/migration/models/TestMigrationModel.js';
-import TestApiTokenModel from '@/tests/larascript/models/models/TestApiTokenModel.js';
-import TestModel from '@/tests/larascript/models/models/TestModel.js';
-import TestUser from '@/tests/larascript/models/models/TestUser.js';
 import testHelper from '@/tests/testHelper.js';
 import { describe } from '@jest/globals';
 import { IDatabaseSchema } from '@larascript-framework/larascript-database';
+import { dropTestMigrationTables, testMigrationTables } from './migrations/test-migration.js';
 
 const dropAndCreateMigrationSchema = async () => {
     const migrationTable = new TestMigrationModel(null).table
@@ -18,40 +16,24 @@ const dropAndCreateMigrationSchema = async () => {
     await app('db').createMigrationSchema(migrationTable)
 }
 
-const dropTestSchema = async () => {
-    if (await app('db').schema().tableExists('tests')) {
-        await app('db').schema().dropTable('tests');
-    }
-
-}
-
-
 describe('test migrations', () => {
 
     let schema: IDatabaseSchema;
 
-    let tables: string[];
-
     beforeAll(async () => {
         await testHelper.testBootApp()
-
-        tables = [
-            TestApiTokenModel.getTable(),
-            TestUser.getTable(),
-            TestModel.getTable()
-        ]
 
         console.log('Connection', app('db').getDefaultConnectionName())
 
         await dropAndCreateMigrationSchema()
 
-        await dropTestSchema()
+        await dropTestMigrationTables()
 
         schema = app('db').schema();
     });
 
     afterAll(async () => {
-        await app('db').schema().dropTable('tests');
+        await dropTestMigrationTables()
         await app('db').schema().dropTable('migrations');
     })
 
@@ -59,7 +41,7 @@ describe('test migrations', () => {
 
         await app('console').readerService(['migrate:up', '--group=testing']).handle();
 
-        for (const table of tables) {
+        for (const table of testMigrationTables()) {
             console.log('checking if table exists', table)
             const tableExists = await schema.tableExists(table);
             console.log('tableExists (expect: true)', table, tableExists)
@@ -71,7 +53,7 @@ describe('test migrations', () => {
 
         await app('console').readerService(['migrate:down', '--group=testing']).handle();
 
-        for (const table of tables) {
+        for (const table of testMigrationTables()) {
             const tableExists = await schema.tableExists(table);
             console.log('tableExists (expect: false)', table, tableExists)
             expect(tableExists).toBe(false);
--- apps/larascript-framework/src/tests/larascript/migration/migrations/test-migration.ts
+++ apps/larascript-framework/src/tests/larascript/migration/migrations/test-migration.ts
@@ -1,20 +1,30 @@
 import { app } from "@/core/services/App.js";
+import TestModel from "@/tests/larascript/models/models/TestModel.js";
 import { BaseMigration } from "@larascript-framework/larascript-database";
 import { DataTypes } from "sequelize";
+
+export const testMigrationTables = () => [
+    TestModel.getTable()
+]
+
+export const dropTestMigrationTables = async () => {
+    if(await app('db').schema().tableExists(TestModel.getTable())) {
+        await app('db').schema().dropTable(TestModel.getTable())
+    }
+}
 
 class TestMigration extends BaseMigration {
 
     group?: string = 'testing';
 
     async up(): Promise<void> {
-        await app('db').schema().createTable('tests', {
-            name: DataTypes.STRING,
-            age: DataTypes.INTEGER
+        await app('db').schema().createTable(TestModel.getTable(), {
+            name: DataTypes.STRING
         })
     }
 
     async down(): Promise<void> {
-        await app('db').schema().dropTable('tests')
+        await app('db').schema().dropTable(TestModel.getTable())
     }
 
 }
